c2a4b9f9c365ef72469d8e5baa5b6f03b17a2660
AngelPradeepa <pradeeangel@gmail.com>
2023-10-09 13:08:02 +0530

    add updated code

--- Seleniumnopcommproject/src/test/java/runner/RunnerClass.java
+++ Seleniumnopcommproject/src/test/java/runner/RunnerClass.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features= {"C:\\Users\\lego_bravin\\eclipse-workspace\\Seleniumnopcommproject\\src\\test\\resources\\Feature Files"},
-		glue="",tags ="",dryRun=false,monochrome = true,plugin = {"pretty","html:target/cucumber-reports/report.html","rerun:target1/failedrerun.txt","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})	
+		glue="",tags ="@Regression", dryRun=false,monochrome = true,plugin = {"pretty","html:target/cucumber-reports/report.html","rerun:target1/failedrerun.txt","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})	
 
 public class RunnerClass {
 
